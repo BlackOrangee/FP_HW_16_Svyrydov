bfa7c5f3ed19b1622f5e1f809a951a71d5f4e242
BlackOrangee <doktorzoidberg8@gmail.com>
2023-04-11 17:37:29 +0300

    Write a function that determines the arithmetic mean of the elements of the array passed to it

--- Task_2/Task_2.cpp
+++ Task_2/Task_2.cpp
@@ -4,7 +4,45 @@
 #include <iostream>
 using namespace std;
 
+int Average(int arr[], int SIZE);
+void OutputArray(int arr[], int SIZE);
+void GenerateArray(int arr[], int SIZE);
+
 int main()
 {
+	const int SIZE = 10;
+	int arr[SIZE];
 
+	GenerateArray(arr, SIZE);
+	OutputArray(arr, SIZE);
+
+	cout << "\n\n\tAverage: " << Average(arr, SIZE) << "\n\n\n";
+}
+
+int Average(int arr[], int SIZE) //Calculate array average
+{
+	int sum = 0;
+	for (int i = 0; i < SIZE; i++)
+	{
+		sum += arr[i];
+	}
+	return sum / SIZE;
+}
+
+void OutputArray(int arr[], int SIZE) //Output array
+{
+	cout << "\n\n\t";
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << " ";
+	}
+}
+
+void GenerateArray(int arr[], int SIZE) // Generate array
+{
+	srand(time(NULL));
+	for (int i = 0; i < SIZE; i++)
+	{
+		arr[i] = rand() % 10;
+	}
 }
